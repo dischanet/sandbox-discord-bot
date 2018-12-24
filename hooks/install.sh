3a779915148dcdd535f480a9668d740b9f7b998f
Yusuke Hayashi <yusuke8h@gmail.com>
2018-12-24 18:49:50 +0900

    Update

--- hooks/install.sh
+++ hooks/install.sh
@@ -2,6 +2,10 @@
 
 cd /home/centos/repos/sandbag-discord-bot
 
-sudo ./hooks/set-environment.sh
-sudo ln -s ./hooks/sandbag-discord-bot.service /etc/systemd/system/sandbag-discord-bot.service
-sudo /usr/bin/systemctl enable sandbag-discord-bot
+ZONE=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone)
+REGION=$(echo ${ZONE/%?/})
+PARAMETER_NAME=sandbag_DISCORD_BOT_SECRET
+
+echo "DISCORD_BOT_TOKEN=$(/home/centos/.pyenv/shims/aws --region ${REGION} ssm get-parameters --name ${PARAMETER_NAME} --query "Parameters[0].Value" --output text)" > environment
+
+sudo cp ./hooks/sandbag-discord-bot.service /etc/systemd/system/sandbag-discord-bot.service
