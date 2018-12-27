9a789dcbe4c82a0d430632e19001a357da9307fa
Yusuke Hayashi <yusuke8h@gmail.com>
2018-12-27 23:09:22 +0900

    デプロイ先を変更

--- hooks/install.sh
+++ hooks/install.sh
@@ -2,10 +2,8 @@
 
 cd /home/centos/repos/sandbox-discord-bot
 
-ZONE=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone)
-REGION=$(echo ${ZONE/%?/})
-PARAMETER_NAME=_DISCORD_BOT_SECRET
-
-echo "DISCORD_BOT_TOKEN=$(/home/centos/.pyenv/shims/aws --region ${REGION} ssm get-parameters --name ${PARAMETER_NAME} --query "Parameters[0].Value" --output text)" > environment
+REGION=$(curl -s 169.254.169.254/latest/meta-data/local-hostname | cut -d '.' -f2)
+PARAMETER_NAME=SANDBOX_DISCORD_BOT_SECRET
+echo "DISCORD_BOT_TOKEN=$(/usr/bin/aws --region ${REGION} ssm get-parameter --name ${PARAMETER_NAME} --query "Parameter.Value" --output text)" > environment
 
 sudo cp ./hooks/sandbox-discord-bot.service /etc/systemd/system/sandbox-discord-bot.service
